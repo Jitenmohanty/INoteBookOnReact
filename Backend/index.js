a722c4f50c1243e81d647110c6d925c214904a7f
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-13 18:45:42 +0530

    Cors added

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,10 @@ const port = 3005;
 
 //It is a middleware to request showing on console..
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:"*",
+  credentials:true,
+}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
